7e6c275d702b264d01a0e1b104e61d08c4963b4c
Max Hauser <hauser.m97@gmail.com>
2020-11-03 17:19:10 +0100

    added code from frontend programming

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/*
+Onderwerp: Wat is de beste parkeerplek voor een getaway car
+
+Criteria:
+1. Je betaald met cash of gratis
+2. Goudstaven zijn zwaar dus het moet een (grote) auto zijn
+3. Het moet dicht bij een snelweg zijn
+4. Je moet ten alle tijden weg kunnen rijden
+5. De parkeerplek moet niet te groot zijn anders duurt het lang voor je kan ontsnappen
+
+*/
+
+// This file calls the init script!
+import init from "../modules/criteria/init.js"
+init();
